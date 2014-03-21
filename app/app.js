ebfbe6733f3ed745b12e45d7e4c880d8fe45f314
Diego <diegofss11@gmail.com>
2014-03-20 23:48:31 -0300

    Change title - Routing

--- app/app.js
+++ app/app.js
@@ -1,5 +1,9 @@
 var moduleApp = angular.module('concertApp', ['ngRoute']);
 
+moduleApp.run(['$rootScope', function($rootScope){
+    $rootScope.title = 'hehe';
+}]);
+
 //This configures the routes and associates each route with a view and a controller
 moduleApp.config(function ($routeProvider) {
     $routeProvider
@@ -7,26 +11,30 @@ moduleApp.config(function ($routeProvider) {
             {
                 controller: 'HomePageController',
                 templateUrl: '/app/views/mainView.html'
-            })
+			})
 		.when('/createConcert',
             {
                 controller: 'CreateConcertController',
-                templateUrl: '/app/views/createConcertView.html'
+                templateUrl: '/app/views/createConcertView.html',
+				title: 'Create'
             })
         .when('/editConcert/:concertId',
             {
                 controller: 'EditConcertController',
-                templateUrl: '/app/views/concertDetailView.html'
+                templateUrl: '/app/views/concertDetailView.html',
+				title: 'Edit'
             })
 		.when('/editConcert/',
             {
                 controller: 'SearchConcertController',
-                templateUrl: '/app/views/editConcertView.html'
+                templateUrl: '/app/views/editConcertView.html',
+				title: 'Edit'
             })
         .when('/searchConcert',
             {
                 controller: 'SearchConcertController',
-                templateUrl: '/app/views/searchConcertView.html'
+                templateUrl: '/app/views/searchConcertView.html',
+				title: 'Search'
             })		
         .otherwise({ redirectTo: '/' });
 })
@@ -35,3 +43,8 @@ moduleApp.config(function ($routeProvider) {
       $(document).foundation();
     });
   });
+  
+$rootScope.$on("$routeChangeSuccess", function(currentRoute, previousRoute){
+    //Change page title, based on Route information
+    $rootScope.title = $route.current.title;
+});
---
+++ app/controllers/HomePageController.js
@@ -0,0 +1,5 @@
+//This controller retrieves data from the concertService and associates it with the $scope
+//The $scope is ultimately bound to the customers view
+moduleApp.controller('HomePageController', function ($scope, concertService) {
+
+});
